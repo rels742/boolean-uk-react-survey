1a548410322a931bd25e3248636d9e13657f699a
Lorel <lorel.oboue@gmail.com>
2022-07-21 17:31:21 +0100

    update

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,6 +2,13 @@ import { useState } from "react";
 
 function Main() {
   const [open, setOpen] = useState(false); //Ignore this state
+  const [answersList, setAnswersList] = useState([]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    console.log("I'm submiting");
+  };
 
   return (
     <main className="main">
@@ -9,7 +16,53 @@ function Main() {
         <h2>Answers list</h2>
         {/* answers should go here */}
       </section>
-      <section className="main__form">{/* a form should be here */}</section>
+      <section className="main__form">
+        {" "}
+        <form class="form" onSubmit={handleSubmit}>
+          <h2>Tell us what you think about your rubber duck!</h2>
+          <div class="form__group radio">
+            <h3>How do you rate your rubber duck colour?</h3>
+            {/* <!-- Radio inputs go here --> */}
+            <ul>
+              <li>
+                <input id="color-one" type="radio" name="color" value="1" />
+                <label for="color-one">1</label>
+              </li>
+              <li>
+                <input id="color-two" type="radio" name="color" value="2" />
+                <label for="color-two">2</label>
+              </li>
+              <li>
+                <input id="color-three" type="radio" name="color" value="3" />
+                <label for="color-three">3</label>
+              </li>
+              <li>
+                <input id="color-four" type="radio" name="color" value="4" />
+                <label for="color-four">4</label>
+              </li>
+            </ul>
+            // onChange on inputs // a function to handle the onChange events
+            that handle the state // a state -
+          </div>
+          <div class="form__group">
+            <h3>How do you like to spend time with your rubber duck</h3>
+            {/* <!-- checkboxes go here --> */}
+          </div>
+          <label>
+            What else have you got to say about your rubber duck?
+            <textarea name="review" cols="30" rows="10"></textarea>
+          </label>
+          <label>
+            Put your name here (if you feel like it):
+            <input type="text" name="username" value="" />
+          </label>
+          <label>
+            Leave us your email pretty please??
+            <input type="email" name="email" value="" />
+          </label>
+          <input class="form__submit" type="submit" value="Submit Survey!" />
+        </form>
+      </section>
     </main>
   );
 }
